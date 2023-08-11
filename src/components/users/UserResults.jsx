fbb954d37cd424e61d2ee98eb474ab9caff49f7f
KQuiggins <kashfamilyvalues@gmail.com>
2023-08-11 09:12:05 -0400

    added loading spinner

--- src/components/users/UserResults.jsx
+++ src/components/users/UserResults.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from 'react';
+import Spinner from '../layout/Spinner';
 
 const UserResults = () => {
 
@@ -20,14 +21,20 @@ const UserResults = () => {
 
         setUsers(data);
         setLoading(false);
-
-
     }
 
+    if (!loading) {
+        return (
+            <div className='grid grid-cols-1 gap-8 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2'>{users.map((user) => (
+                <h3 key={user.id}>{user.login}</h3>
+            ))}</div>
+          )
+    } else {
+        return (
+            <Spinner />
+        )
+    }
 
-  return (
-    <div>UserResults</div>
-  )
 }
 
 export default UserResults
